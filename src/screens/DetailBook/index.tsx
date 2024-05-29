d5cd762740cd8369cedc5e4182bfd5c087e27d02
yahoogm <yaho.garfield@gmail.com>
2024-05-29 18:47:56 +0700

    style: create new view for detail book

--- src/screens/DetailBook/index.tsx
+++ src/screens/DetailBook/index.tsx
@@ -1,4 +1,12 @@
-import {Text} from '@gluestack-ui/themed';
+import {
+  Button,
+  ButtonText,
+  Heading,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from '@gluestack-ui/themed';
 import {RootStackParamList} from '../../types/types';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {useEffect} from 'react';
@@ -16,7 +24,7 @@ const DetailBook = () => {
     );
 
     const data = await req.data;
-    console.log(data);
+    console.log(data.volumeInfo.title);
 
     return data;
   };
@@ -26,7 +34,63 @@ const DetailBook = () => {
   }, []);
 
   console.log(bookId);
-  return <Text>oakwoawkoa</Text>;
+  return (
+    <ScrollView>
+      <View display="flex" alignItems="center" padding={20}>
+        <Image
+          alt="Image"
+          size="2xl"
+          source={{
+            uri: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+          }}
+        />
+
+        <View display="flex" flexDirection="row" gap={10} marginTop={10}>
+          <Button>
+            <ButtonText>Read Book</ButtonText>
+          </Button>
+          <Button>
+            <ButtonText>Add Review</ButtonText>
+          </Button>
+        </View>
+
+        <View alignItems="center" marginTop={10}>
+          <View alignItems="center">
+            <Heading size="md">Author:</Heading>
+            <Text>william</Text>
+          </View>
+          <View alignItems="center">
+            <Heading size="md">Publisher:</Heading>
+            <Text>william</Text>
+          </View>
+          <View alignItems="center">
+            <Heading size="md">Published Date:</Heading>
+            <Text>william</Text>
+          </View>
+          <View alignItems="center">
+            <Heading size="md">Page Count:</Heading>
+            <Text>william</Text>
+          </View>
+          <View alignItems="center">
+            <Heading size="md">Category:</Heading>
+            <Text>william</Text>
+          </View>
+          <View alignItems="center">
+            <Heading size="md">Description:</Heading>
+            <Text>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+              officiis, sequi aliquid odio consequatur voluptate aut possimus!
+              Aspernatur dolor architecto ab veniam accusamus tempore possimus
+              nisi eveniet harum. Praesentium ipsam harum laboriosam quasi
+              dolorum placeat non voluptates quidem, ducimus corrupti culpa
+              repudiandae libero provident tenetur aspernatur possimus vero
+              doloremque quod?
+            </Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default DetailBook;
