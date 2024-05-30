314dfb708a263619990f85ab9debd16175d067ee
yahoogm <yaho.garfield@gmail.com>
2024-05-30 18:44:58 +0700

    core: detail book selector

--- src/redux/book/bookSelector.ts
+++ src/redux/book/bookSelector.ts
@@ -2,3 +2,4 @@ import {RootState} from '../store';
 
 export const searchBooks = (state: RootState) => state.book.books;
 export const useSearchBookType = (state: RootState) => state.book.type;
+export const detailBookSelector = (state: RootState) => state.book.detailBook;
