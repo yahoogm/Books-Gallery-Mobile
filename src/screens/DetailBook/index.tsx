327cd1327405171987de400181f6721afb8d44ae
yahoogm <yaho.garfield@gmail.com>
2024-05-31 15:25:31 +0700

    fix: repair view

--- src/screens/DetailBook/index.tsx
+++ src/screens/DetailBook/index.tsx
@@ -2,18 +2,18 @@ import {
   Button,
   ButtonText,
   Heading,
-  Image,
   ScrollView,
   Text,
   View,
+  Image,
 } from '@gluestack-ui/themed';
 import {RootStackParamList} from '../../types/types';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {useEffect} from 'react';
-import axios from 'axios';
 import {useAppDispatch, useAppSelector} from '../../hooks/useRedux';
-import {addDetailBook} from '../../redux/book/bookSlice';
 import {detailBookSelector} from '../../redux/book/bookSelector';
+import {modifiedName} from '../../../utils/const/const';
+import {retrieveDetailBook} from '../../redux/book/bookThunk';
 
 type DetailBooksRouteProp = RouteProp<RootStackParamList, 'DetailBook'>;
 
@@ -23,29 +23,25 @@ const DetailBook = () => {
   const detailBook = useAppSelector(detailBookSelector);
   const {bookId} = route.params;
 
-  const getDetailBook = async () => {
-    const req = await axios.get(
-      `https://www.googleapis.com/books/v1/volumes/${bookId}?=AIzaSyBSAX-msAaEmoJYIae3XsXuOx2oVr7Ta-I`,
-    );
-    const data = await req.data;
-    dispatch(addDetailBook(data));
-  };
-
   useEffect(() => {
-    getDetailBook();
+    dispatch(retrieveDetailBook({bookId: bookId}));
   }, []);
+
+  const authorsName = modifiedName(detailBook?.volumeInfo.authors);
+  const categories = modifiedName(detailBook?.volumeInfo.categories);
 
   return (
     <ScrollView>
       <View display="flex" alignItems="center" padding={20}>
         <Image
-          alt="Image"
-          size="2xl"
+          alt="title"
+          width={250}
+          h={340}
+          borderRadius={10}
           source={{
-            uri:
-              detailBook.volumeInfo.imageLinks.thumbnail !== null
-                ? detailBook.volumeInfo.imageLinks.thumbnail
-                : 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+            uri: detailBook.volumeInfo.imageLinks.thumbnail
+              ? detailBook.volumeInfo.imageLinks.thumbnail
+              : 'https://plus.unsplash.com/premium_photo-1698084059435-a50ddfd69303?q=80&w=1850&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
           }}
         />
 
@@ -61,35 +57,29 @@ const DetailBook = () => {
         <View alignItems="center" marginTop={10}>
           <View alignItems="center">
             <Heading size="md">Author:</Heading>
-            <Text>william</Text>
+            <Text>
+              {authorsName !== null ? authorsName : 'Penulis tidak ditemukan'}
+            </Text>
           </View>
           <View alignItems="center">
             <Heading size="md">Publisher:</Heading>
-            <Text>william</Text>
+            <Text>{detailBook.volumeInfo.publisher}</Text>
           </View>
           <View alignItems="center">
             <Heading size="md">Published Date:</Heading>
-            <Text>william</Text>
+            <Text>{detailBook.volumeInfo.publishedDate}</Text>
           </View>
           <View alignItems="center">
             <Heading size="md">Page Count:</Heading>
-            <Text>william</Text>
+            <Text>{detailBook.volumeInfo.pageCount}</Text>
           </View>
           <View alignItems="center">
             <Heading size="md">Category:</Heading>
-            <Text>william</Text>
+            <Text>{categories}</Text>
           </View>
           <View alignItems="center">
             <Heading size="md">Description:</Heading>
-            <Text>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
-              officiis, sequi aliquid odio consequatur voluptate aut possimus!
-              Aspernatur dolor architecto ab veniam accusamus tempore possimus
-              nisi eveniet harum. Praesentium ipsam harum laboriosam quasi
-              dolorum placeat non voluptates quidem, ducimus corrupti culpa
-              repudiandae libero provident tenetur aspernatur possimus vero
-              doloremque quod?
-            </Text>
+            <Text>{detailBook.volumeInfo.description}</Text>
           </View>
         </View>
       </View>
