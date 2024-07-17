fffee337d8a2cab302c3885d16efa3b11150684e
yahoogm <yaho.garfield@gmail.com>
2024-07-17 14:49:21 +0700

    refactor: create component for main navigator

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {Store} from './src/redux/store';
-import {GluestackUIProvider} from '@gluestack-ui/themed';
-import {config} from '@gluestack-ui/config';
+
 import MainNavigator from './MainNavigator';
-import {NavigationContainer} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
-  return (
-    <Provider store={Store}>
-      <GluestackUIProvider config={config}>
-        <NavigationContainer>
-          <MainNavigator />
-        </NavigationContainer>
-      </GluestackUIProvider>
-    </Provider>
-  );
+  return <MainNavigator />;
 }
 
 export default App;
